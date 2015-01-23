9b555066acab2eef53885c513273c5ff9888c5f3
Jeff Patzer <jeff.patzer@moovweb.com>
2015-01-22 18:39:54 -0800

    add in git url

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Payment library by Stripe. A general purpose library for building credit card forms, validating inputs and formatting numbers.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/jpatzer/meteor-jquery-payment',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
